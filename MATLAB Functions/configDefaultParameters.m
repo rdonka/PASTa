dd7e4acbffc3287ad8424666911cada27ae85066
rdonka <115414724+rdonka@users.noreply.github.com>
2025-03-08 20:28:10 -0600

    Updated input handling and default parameters for extractTDTdata

---
+++ MATLAB Functions/configDefaultParameters.m
@@ -0,0 +1,9 @@
+function defaultparameters = configDefaultParameters()
+% CONFIGDEFAULTPARAMETERS    Returns a struct of all default analysis
+%                            parameters.
+% Copyright (C) 2025 Rachel Donka. Licensed under the GNU General Public License v3.
+
+    defaultparameters.trim = 5;
+    defaultparameters.skipexisting = 1;
+
+end

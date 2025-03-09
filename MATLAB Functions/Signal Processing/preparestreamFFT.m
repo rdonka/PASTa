daf4889d9466af1c086339825b3e1529d4d874a2
rdonka <115414724+rdonka@users.noreply.github.com>
2025-03-09 17:03:55 -0500

    Update preparestreamFFT.m

--- MATLAB Functions/Signal Processing/preparestreamFFT.m
+++ MATLAB Functions/Signal Processing/preparestreamFFT.m
@@ -1,36 +1,43 @@
-function [streamFFT,streamF] = preparestreamFFT(streamdata,fs)
-% PREPARESTREAMFFT  Prepares the one sided FFT of the stream for plotting 
-%                   and analysis.
+function [streamFFT, streamF] = preparestreamFFT(streamdata,fs)
+% PREPARESTREAMFFT  Compute the one-sided FFT of a data stream for analysis and plotting.
 %
-% Copyright (C) 2024 Rachel Donka. Licensed under the GNU General Public License v3.
+%   [streamFFT, streamF] = PREPARESTREAMFFT(streamdata, fs) computes the
+%   one-sided Fast Fourier Transform (FFT) of the input data stream and
+%   returns the FFT magnitudes along with their corresponding frequencies.
 %
-% INPUTS:
-%   STREAMDATA:         Array; Values from a single collected data stream.
+% REQUIRED INPUTS:
+%       STREAMDATA - Numeric vector; the input data stream to be transformed.
 %
-%   FS:                 Sampling rate of the data stream.
+%       FS         - Positive scalar; the sampling rate of the data stream
+%                    in Hz.
 %
-% OUTPUTS:
-%   STREAMFFT:          An array containing the prepared FFT magnitudes of
-%                       the input stream.
+%   OUTPUTS:
+%       STREAMFFT  - Numeric vector; the magnitudes of the one-sided FFT
+%                    of the input data stream.
 %
-%   STREAMF:            Corresponding frequencies to the prepared FFT
-%                       magnitudes in STREAMFFT.
+%       STREAMF    - Numeric vector; the corresponding frequency values in
+%                    Hz for the FFT magnitudes.
 %
-% Stored in the PASTa GitHub Repository, see the user guide for additional
-% documentation: https://rdonka.github.io/PASTa/
+%   EXAMPLE:
+%       % Compute the FFT
+%       [streamFFT, streamF] = preparestreamFFT(streamdata, fs);
+%
+% Author:  Rachel Donka (2025)
+% License: GNU General Public License v3. See end of file for details.
+% Stored in the PASTa GitHub Repository: https://github.com/rdonka/PASTa
+% For detailed instructions, see the PASTa user guide: https://rdonka.github.io/PASTaUserGuide/
 
-    %% Find length of data
-    L = length(streamdata);
-
-    %% Prepare the FFT
+    %% Compute FFT
+    L = length(streamdata); % Length of data
     fft1 = abs((fft(streamdata,L))/L); % Take the fft
     fft2 = fft1(1:floor(L/2)+1); % Take one side
     fft2(2:end-1) = 2*fft2(2:end-1); % Double the power to adjust for one-sided
+    
     streamFFT = fft2; % Output streamFFT
     streamF = fs*(0:floor(L/2))/L; % Output streamF; Spatially matched array frequency labels
 end
 
-% Copyright (C) 2024 Rachel Donka
+% Copyright (C) 2025 Rachel Donka
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
